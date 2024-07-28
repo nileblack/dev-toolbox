8cb98c420e8ddbd53dd438d4f531dc63940265fe
sam <sam.lau@docyt.com>
2024-07-28 11:37:55 +0800

    init

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'dbd8802d4a22405b9e175e93c3fb2ba4',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
